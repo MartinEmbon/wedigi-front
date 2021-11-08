f04b68883b3fe83bdaae8ceedf0c5326c0b111c8
Martincho <martinembon@hotmail.com>
2021-11-08 16:56:45 -0300

    upload

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let indexRouter = require('./routes/indexRoute');
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use("/stylesheets",express.static(path.resolve(__dirname,"public/stylesheets")))
+app.use("/images",express.static(path.resolve(__dirname,"public/images")))
+app.use("/javascript",express.static(path.resolve(__dirname,"aspublicsets/javascript")))
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
